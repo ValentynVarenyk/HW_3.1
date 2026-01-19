ea941460d8e3b7bd01e1c30c50b426ae3c379eef
ValentynVarenyk <varenykvalentyn@gmail.com>
2026-01-19 17:36:00 +0200

    ДЗ

---
+++ js/hw.js
@@ -0,0 +1,39 @@
+// 1
+let age = 12;
+console.log(age);
+
+// 2
+let name = "Валентин";
+console.log(name);
+
+// 3
+let isStudent = "true";
+console.log(isStudent);
+
+// 4
+let myString = "Не кажи спробую, завжди кажи я Зможу!";
+console.log(myString);
+
+// 5
+let myNumber = 13422;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+// 6
+let myNull = "null";
+console.log(myNull);
+
+// 7
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім’я: " + userName);
+console.log(typeof userName);
+
+// 8
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(userAnswer);
+
+// 9
+alert("Увага! Дія може бути небезпечною.");
+let dangerConfirm = confirm("Ви впевнені, що хочете продовжити?");
+console.log("Користувач підтвердив дію:", dangerConfirm);
